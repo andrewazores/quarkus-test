57b175878871b12377c0daab025e8a52a35948a1
Andrew Azores <me@andrewazor.es>
2022-07-26 12:56:01 -0400

    re-attempt registration on initial failure (cryostat not up yet?)

--- src/main/java/org/acme/AppLifecycle.java
+++ src/main/java/org/acme/AppLifecycle.java
@@ -2,6 +2,8 @@ package org.acme;
 
 import java.net.URI;
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -16,28 +18,59 @@ import io.vertx.core.json.JsonObject;
 @ApplicationScoped
 public class AppLifecycle {
 
-    PluginInfo plugin;
+    volatile PluginInfo plugin;
     @Inject @RestClient CryostatService cryostat;
+    Future<?> submission;
 
     void onStart(@Observes StartupEvent ev) {
-        RegistrationInfo registration = new RegistrationInfo();
-        registration.realm = "quarkus-test";
-        registration.callback = "http://localhost/unimplemented-callback";
-        JsonObject response = cryostat.register(registration);
-        this.plugin = response.getJsonObject("data").getJsonObject("result").mapTo(PluginInfo.class);
+        this.submission = Executors.newSingleThreadExecutor().submit(() -> {
+            while (plugin == null) {
+                try {
+                    RegistrationInfo registration = new RegistrationInfo();
+                    registration.realm = "quarkus-test";
+                    registration.callback = "http://localhost/unimplemented-callback";
+                    JsonObject response = cryostat.register(registration);
+                    this.plugin = response.getJsonObject("data").getJsonObject("result").mapTo(PluginInfo.class);
 
-        Node selfNode = new Node();
-        selfNode.nodeType = "JVM";
-        selfNode.name = "quarkus-test-" + this.plugin.id;
-        selfNode.target = new Node.Target();
-        selfNode.target.alias = "quarkus-test-1";
-        selfNode.target.connectUrl = URI.create("service:jmx:rmi:///jndi/rmi://cryostat:9097/jmxrmi");
-        cryostat.update(this.plugin.id, this.plugin.token, Set.of(selfNode));
+                    Node selfNode = new Node();
+                    selfNode.nodeType = "JVM";
+                    selfNode.name = "quarkus-test-" + this.plugin.id;
+                    selfNode.target = new Node.Target();
+                    selfNode.target.alias = "quarkus-test-1";
+                    selfNode.target.connectUrl = URI.create("service:jmx:rmi:///jndi/rmi://cryostat:9097/jmxrmi");
+                    cryostat.update(this.plugin.id, this.plugin.token, Set.of(selfNode));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    deregister();
+                }
+
+                if (this.plugin != null) {
+                    break;
+                }
+                try {
+                    Thread.sleep(5_000);
+                } catch (InterruptedException ie) {
+                    ie.printStackTrace();
+                    break;
+                }
+            }
+        });
     }
 
     void onStop(@Observes ShutdownEvent ev) {
+        if (this.submission != null) {
+            this.submission.cancel(true);
+        }
+        deregister();
+    }
+
+    private void deregister() {
         if (plugin != null) {
-            cryostat.deregister(this.plugin.id, this.plugin.token);
+            try {
+                cryostat.deregister(this.plugin.id, this.plugin.token);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
