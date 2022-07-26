d806a5620fe6774a984b9ab371f97d0c9094751f
Andrew Azores <me@andrewazor.es>
2022-07-26 13:20:43 -0400

    get values from properties and config, not hardcoded

--- src/main/java/org/acme/AppLifecycle.java
+++ src/main/java/org/acme/AppLifecycle.java
@@ -2,6 +2,7 @@ package org.acme;
 
 import java.net.URI;
 import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
@@ -9,6 +10,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import io.quarkus.runtime.ShutdownEvent;
@@ -21,6 +23,8 @@ public class AppLifecycle {
     volatile PluginInfo plugin;
     @Inject @RestClient CryostatService cryostat;
     Future<?> submission;
+
+    @ConfigProperty(name = "quarkus.application.name") String appName;
 
     void onStart(@Observes StartupEvent ev) {
         this.submission = Executors.newSingleThreadExecutor().submit(() -> {
@@ -36,8 +40,13 @@ public class AppLifecycle {
                     selfNode.nodeType = "JVM";
                     selfNode.name = "quarkus-test-" + this.plugin.id;
                     selfNode.target = new Node.Target();
-                    selfNode.target.alias = "quarkus-test-1";
-                    selfNode.target.connectUrl = URI.create("service:jmx:rmi:///jndi/rmi://cryostat:9097/jmxrmi");
+                    selfNode.target.alias = appName;
+
+                    String hostname = System.getProperty("java.rmi.server.hostname", "localhost");
+                    int jmxport = Integer.valueOf(System.getProperty("com.sun.management.jmxremote.port", "9097"));
+
+                    selfNode.target.connectUrl = URI.create(String.format("service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi", hostname, jmxport));
+                    System.out.println("registering self as " + selfNode.target.connectUrl);
                     cryostat.update(this.plugin.id, this.plugin.token, Set.of(selfNode));
                 } catch (Exception e) {
                     e.printStackTrace();
