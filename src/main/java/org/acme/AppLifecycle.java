4bb7f6d293510d486760a96daef8cd621f1dae75
Andrew Azores <me@andrewazor.es>
2022-07-26 13:58:55 -0400

    replace executorservice with vertx periodic/executeBlocking

--- src/main/java/org/acme/AppLifecycle.java
+++ src/main/java/org/acme/AppLifecycle.java
@@ -2,8 +2,6 @@ package org.acme;
 
 import java.net.URI;
 import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
@@ -15,72 +13,92 @@ import org.jboss.logging.Logger;
 
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 
 @ApplicationScoped
 public class AppLifecycle {
 
-    volatile PluginInfo plugin;
+    @Inject Vertx vertx;
     @Inject @RestClient CryostatService cryostat;
+    volatile PluginInfo plugin;
+    long submission = Long.MIN_VALUE;
     @Inject Logger log;
-    Future<?> submission;
 
     @ConfigProperty(name = "quarkus.application.name") String appName;
 
     void onStart(@Observes StartupEvent ev) {
-        this.submission = Executors.newSingleThreadExecutor().submit(() -> {
-            while (plugin == null) {
-                try {
-                    RegistrationInfo registration = new RegistrationInfo();
-                    registration.realm = "quarkus-test";
-                    registration.callback = "http://localhost/unimplemented-callback";
-                    JsonObject response = cryostat.register(registration);
-                    this.plugin = response.getJsonObject("data").getJsonObject("result").mapTo(PluginInfo.class);
-
-                    Node selfNode = new Node();
-                    selfNode.nodeType = "JVM";
-                    selfNode.name = "quarkus-test-" + this.plugin.id;
-                    selfNode.target = new Node.Target();
-                    selfNode.target.alias = appName;
-
-                    String hostname = System.getProperty("java.rmi.server.hostname", "localhost");
-                    int jmxport = Integer.valueOf(System.getProperty("com.sun.management.jmxremote.port", "9097"));
-
-                    selfNode.target.connectUrl = URI.create(String.format("service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi", hostname, jmxport));
-                    log.info("registering self as " + selfNode.target.connectUrl);
-                    cryostat.update(this.plugin.id, this.plugin.token, Set.of(selfNode));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    deregister();
-                }
-
-                if (this.plugin != null) {
-                    break;
-                }
-                try {
-                    Thread.sleep(5_000);
-                } catch (InterruptedException ie) {
-                    ie.printStackTrace();
-                    break;
-                }
+        tryRegister();
+        this.submission = vertx.setPeriodic(5_000, id -> {
+            tryRegister();
+            if (this.plugin != null) {
+                vertx.cancelTimer(id);
             }
         });
     }
 
+    private void tryRegister() {
+        if (this.plugin != null) {
+            return;
+        }
+        vertx.<PluginInfo>executeBlocking(promise -> {
+            try {
+                RegistrationInfo registration = new RegistrationInfo();
+                registration.realm = "quarkus-test";
+                registration.callback = "http://localhost/unimplemented-callback";
+                JsonObject response = cryostat.register(registration);
+                PluginInfo plugin = response.getJsonObject("data").getJsonObject("result").mapTo(PluginInfo.class);
+
+                Node selfNode = new Node();
+                selfNode.nodeType = "JVM";
+                selfNode.name = "quarkus-test-" + plugin.id;
+                selfNode.target = new Node.Target();
+                selfNode.target.alias = appName;
+
+                String hostname = System.getProperty("java.rmi.server.hostname", "localhost");
+                int jmxport = Integer.valueOf(System.getProperty("com.sun.management.jmxremote.port", "9097"));
+
+                selfNode.target.connectUrl = URI.create(String.format("service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi", hostname, jmxport));
+                log.info("registering self as " + selfNode.target.connectUrl);
+                cryostat.update(plugin.id, plugin.token, Set.of(selfNode));
+
+                promise.complete(plugin);
+            } catch (Exception e) {
+                promise.fail(e);
+            }
+        }, result -> {
+            if (result.failed()) {
+                log.warn(result.cause());
+                result.cause().printStackTrace();
+                deregister();
+                return;
+            }
+            this.plugin = result.result();
+        });
+    }
+
     void onStop(@Observes ShutdownEvent ev) {
-        if (this.submission != null) {
-            this.submission.cancel(true);
+        if (this.submission != Long.MIN_VALUE) {
+            this.vertx.cancelTimer(this.submission);
         }
         deregister();
     }
 
     private void deregister() {
-        if (plugin != null) {
-            try {
-                cryostat.deregister(this.plugin.id, this.plugin.token);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if (this.plugin != null) {
+            vertx.executeBlocking(promise -> {
+                try {
+                    cryostat.deregister(this.plugin.id, this.plugin.token);
+                    promise.complete();
+                } catch (Exception e) {
+                    promise.fail(e);
+                }
+            }, result -> {
+                if (result.failed()) {
+                    log.warn(result.cause());
+                    result.cause().printStackTrace();
+                }
+            });
         }
     }
 
