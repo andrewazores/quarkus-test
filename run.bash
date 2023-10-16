12cef17484e9446abd8d06f44fa37a3419f26068
Andrew Azores <aazores@redhat.com>
2023-10-16 17:31:49 -0400

    use dynamically-attached agent

--- run.bash
+++ run.bash
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-podman run --rm quay.io/andrewazores/quarkus-test:latest
+function cleanup() {
+  podman stop quarkus-test
+}
+trap cleanup EXIT
+
+function loadAgent() {
+  sleep 3
+  podman exec quarkus-test java -jar /deployments/app/cryostat-agent-shaded.jar
+}
+
+podman run --name quarkus-test --rm -d quay.io/andrewazores/quarkus-test:latest
+loadAgent &
+podman logs -f quarkus-test
